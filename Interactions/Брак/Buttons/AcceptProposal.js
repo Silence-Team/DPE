8f1d32bd205b825c30c4a1049e50c88935de14af
EvanQube <evanenev@outlook.com>
2023-01-14 18:29:57 +0300

    Changed accept proposal process Added exception if an invoker or a member already marriaged

--- Interactions/Брак/Buttons/AcceptProposal.js
+++ Interactions/Брак/Buttons/AcceptProposal.js
@@ -1,13 +1,36 @@
 const Members = require('../../../Schemas/Members')
+const CheckMarriage = require('../Functions/CheckMarriage')
 
 module.exports = {
   run: async (interaction) => {
+    await interaction.deferUpdate()
     const users = interaction.message.mentions.parsedUsers.toJSON()
 
     if (interaction.user !== users[0]) {
       return await interaction.reply({
         content: '**Ты не можешь использовать это**',
         ephemeral: true,
+      })
+    }
+
+    const InvokerData = await Members.findOne({ id: users[1].id })
+    const MemberData = await Members.findOne({ id: interaction.member.id })
+
+    if (CheckMarriage(InvokerData)) {
+      return await interaction.update({
+        content:
+          interaction.message.content +
+          `\n\n${users[1]} уже в браке с <@${InvokerData.marriage.id}>`,
+        components: [],
+      })
+    }
+
+    if (CheckMarriage(MemberData)) {
+      return await interaction.update({
+        content:
+          interaction.message.content +
+          `\n\n${interaction.member} уже в браке с <@${MemberData.marriage.id}>`,
+        components: [],
       })
     }
 
@@ -19,36 +42,37 @@ module.exports = {
     const gif =
       gifs[Math.floor(Math.random() * gifs.length)].media_formats.gif.url
 
-    await interaction.update({
+    await interaction.editReply({
       content: `💍 ${interaction.member} и ${users[1]} теперь в браке`,
       components: [],
       files: [gif],
     })
 
-    const InvokerData = await Members.findOne({ id: users[1].id })
-    const MemberData = await Members.findOne({ id: interaction.member.id })
-
-    let InvokerPassport = InvokerData?.passport || {}
-    InvokerPassport.marriage = {
-      id: interaction.member.id,
-      timestamp: Math.floor(Date.now() / 1000),
-    }
-
-    let MemberPassport = MemberData?.passport || {}
-    MemberPassport.marriage = {
-      id: users[1].id,
-      timestamp: Math.floor(Date.now() / 1000),
-    }
+    const timestamp = Math.floor(Date.now() / 1000)
 
     await Members.findOneAndUpdate(
       { id: users[1].id },
-      { passport: InvokerPassport },
+      {
+        $set: {
+          'passport.marriage': {
+            id: users[1].id,
+            timestamp,
+          },
+        },
+      },
       { upsert: true }
     )
 
     await Members.findOneAndUpdate(
       { id: interaction.member.id },
-      { passport: MemberPassport },
+      {
+        $set: {
+          'passport.marriage': {
+            id: interaction.member.id,
+            timestamp,
+          },
+        },
+      },
       { upsert: true }
     )
   },
