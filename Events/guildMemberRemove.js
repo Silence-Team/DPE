c095dd24729dede167b04dfdf7d5d0de3b2e0b9e
EvanQube <evanenev@outlook.com>
2023-01-14 18:31:29 +0300

    Divorce members on leave one of them

--- Events/guildMemberRemove.js
+++ Events/guildMemberRemove.js
@@ -1,12 +1,11 @@
 const { EmbedBuilder } = require('discord.js')
 const ParseUnix = require('../Util/ParseUnix')
 const config = require('../Util/Config')
+const Members = require('../Schemas/Members')
+const { channels } = require('../Util/Config')
 
 module.exports = {
   run: async (member) => {
-    // if (member.joinedTimestamp - member.user.createdTimestamp <= 86_400_000)
-    //   return
-
     const channel = member.guild.channels.cache.get(
       config.channels.logs.members
     )
@@ -37,6 +36,41 @@ module.exports = {
         value: `<t:${ParseUnix(member.joinedTimestamp)}:R>`,
         inline: true,
       })
+    }
+
+    const InvokerData = await Members.findOne({ id: member.id })
+    const InvokerPassport = InvokerData?.passport || {}
+
+    if (Object.keys(InvokerPassport)?.length) {
+      await Members.findOneAndUpdate(
+        { id: member.id },
+        { $unset: { 'passport.marriage': '' } }
+      )
+
+      const MemberID = InvokerPassport.marriage.id
+
+      const MemberData = await Members.findOne({
+        id: MemberID,
+      })
+
+      const MemberPassport = MemberData?.passport || {}
+
+      if (Object.keys(InvokerPassport)?.length) {
+        await Members.findOneAndUpdate(
+          { id: MemberID },
+          { $unset: { 'passport.marriage': '' } }
+        )
+      }
+
+      if (Object.keys(MemberPassport?.marriage || {})?.length) {
+        const MarriagesChannel = member.guild.channels.cache.get(
+          channels.marriages
+        )
+
+        await MarriagesChannel.send({
+          content: `🛑 ${member} и <@${MemberData.id}> больше не в браке`,
+        })
+      }
     }
 
     await channel.send({ embeds: [embed] })
